66adf7fe9204f60633d918cafb1aa57d9302c70b
Matias Capeletto <matias.capeletto@gmail.com>
2020-08-19 08:22:15 +0200

    refactor: use script setup in GridEditor component

--- src/components/grid/GridEditor.vue
+++ src/components/grid/GridEditor.vue
@@ -1,11 +1,12 @@
 <template>
   <section
+    ref="sectionElement"
     :class="{ active: isActive, dragging }"
     :style="{
       'grid-template-rows': grid.row.sizes.join(' '),
       'grid-template-columns': grid.col.sizes.join(' '),
       'grid-gap': grid.row.gap + ' ' + grid.col.gap,
-      display: 'grid'
+      display: 'grid',
     }"
     class="grid"
   >
@@ -20,7 +21,7 @@
     />
 
     <cell
-      v-for="(section, i) in sections"
+      v-for="(section, i) in gridSections(grid)"
       :key="`section-${i}`"
       :area="area"
       :section="section"
@@ -51,7 +52,7 @@
   </section>
 </template>
 
-<script>
+<script setup="props, { el }">
 import IconRemove from '../icons/icon-remove.vue'
 import IconClear from '../icons/icon-clear.vue'
 import IconSubgrid from '../icons/icon-subgrid.vue'
@@ -62,40 +63,35 @@ import AreaEditor from '../AreaEditor.vue'
 import LineName from './LineName.vue'
 
 import { store } from '../../store.js'
-import { gridSections } from '../../utils.js'
+
+export { gridSections } from '../../utils.js'
 
 export default {
-  name: 'GridEditor',
   components: { IconRemove, IconClear, IconSubgrid, Cell, AreaSelection, AreaEditor, LineName },
   props: {
     area: { type: Object, required: true },
     currentArea: { type: Object, required: true },
     currentItem: { type: Number, default: null },
-    parentActive: { type: Boolean, default: false }
+    parentActive: { type: Boolean, default: false },
   },
-  computed: {
-    grid() {
-      return this.area.grid
-    },
-    isActive() {
-      return this.area.name === this.currentArea.name
-    },
-    sections() {
-      return gridSections(this.grid)
-    },
-    dragging() {
-      return store.data.dragging
-    }
-  },
-  methods: {
-    gridComputedStyles() {
-      return window.getComputedStyle(this.$el)
-    },
-    removeArea(area) {
-      const { areas } = this.grid
-      areas.splice(areas.indexOf(area), 1)
-    }
-  }
+}
+
+import { ref, computed } from 'vue'
+
+export const grid = computed(() => props.area.grid)
+
+export const isActive = computed(() => props.area.name === props.currentArea.name)
+
+export const { dragging } = store.data
+
+export const sectionElement = ref(null)
+export function gridComputedStyles() {
+  return window.getComputedStyle(sectionElement.value)
+}
+
+export function removeArea(area) {
+  const { areas } = grid.value
+  areas.splice(areas.indexOf(area), 1)
 }
 </script>
 
