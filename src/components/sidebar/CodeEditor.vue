e9e87efcccc6f0adbb5ed2dd98429669beec87ec
Matias Capeletto <matias.capeletto@gmail.com>
2020-08-21 13:55:03 +0200

    refactor: lazy load prism with new CodeView component

--- src/components/sidebar/CodeEditor.vue
+++ src/components/sidebar/CodeEditor.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="code-container">
     <span class="header">{{ type.toUpperCase() }}</span>
-    <pre><code v-html="highlight(code,type)" /></pre>
+    <suspense>
+      <template #default>
+        <code-view :code="code" :type="type" />
+      </template>
+      <template #fallback>
+        <pre>{{code}}</pre>
+      </template>
+    </suspense>
   </div>
 </template>
 
 <script setup>
-export { highlight } from '../utils/highlight.js'
+// Lazy load CodeView component that includes prism.js and related css
+
+import { defineAsyncComponent } from 'vue'
 
 export default {
+  components: {
+    CodeView: defineAsyncComponent(() => import('./CodeView.vue')),
+  },
   props: {
     type: { type: String, required: true },
     code: { type: String, required: true },
@@ -34,16 +46,20 @@ export default {
     border-top-left-radius: 4px;
     border-top-right-radius: 4px;
   }
-  pre {
-    border: 0;
-    resize: none;
-    overflow: auto;
-    white-space: pre-wrap;
-    text-align: left;
-    padding: 0;
-    margin: 0;
-    font-size: 14px;
-    padding: 10px;
-  }
+}
+
+pre {
+  margin: 0px;
+  padding: 1em;
+  color: #9cdcfe;
+  font-size: 13px;
+  font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace;
+  direction: ltr;
+  text-align: left;
+  white-space: pre;
+  word-spacing: normal;
+  word-break: normal;
+  line-height: 1.5;
+  tab-size: 4;
 }
 </style>
