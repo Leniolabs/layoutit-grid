5f4c1ea1e57a67ce186277d86946d3a8c8624850
benjamin.kugler <benjamin.kugler@elliemae.com>
2020-09-25 17:36:22 -0300

    feat: moved add button for columns and rows to the top

---
+++ src/components/icons/IconAdd.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    version="1.1"
+    x="0"
+    y="0"
+    width="512"
+    height="512"
+    viewBox="0 0 512 512"
+    enable-background="new 0 0 512 512"
+    xml:space="preserve"
+  >
+    <polygon
+      points="512 120.9 391.1 0 256 135.1 120.9 0 0 120.9 135.1 256 0 391.1 120.9 512 256 376.9 391.1 512 512 391.1 376.9 256 "
+    />
+  </svg>
+</template>
+<style lang="scss" scoped>
+svg {
+  transform: rotate(45deg);
+}
+</style>
