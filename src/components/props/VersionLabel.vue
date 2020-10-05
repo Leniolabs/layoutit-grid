42a704c612de669a1806ffdda6113a7b47491e5d
Mariana Beldi <mariana.beldi@leniolabs.com>
2020-10-05 18:17:39 -0300

    version text simplify

--- src/components/props/VersionLabel.vue
+++ src/components/props/VersionLabel.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="version">
     <span>Hire us!</span>
-    <span class="version-text">version 1.2.3</span>
+    <span class="version-text">v1.2.3</span>
   </div>
 </template>
 
@@ -10,10 +10,10 @@
   align-items: flex-end;
   color: #ccc;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   font-size: 1rem;
   font-family: monospace;
-  padding: 6px 0;
+  padding: 6px 14px;
   text-align: center;
   z-index: 99;
   &:hover {
