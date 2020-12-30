9ce9485ea4b0f963337b28e6acc0cd5e1c333cb6
Matias Capeletto <matias.capeletto@gmail.com>
2020-12-30 21:21:41 +0100

    style: remove transparencies from areas

--- src/components/area/AreaEditor.vue
+++ src/components/area/AreaEditor.vue
@@ -12,10 +12,11 @@
       'touch-action': 'none',
       position: 'relative',
       outline: area === currentArea ? `2px solid ${area.color}` : 'none',
-      background:
-        area.display === 'block' && area.parent
+      background: '#ffffff',
+      /*
+        (area.display === 'block' && area.parent)
           ? '#ffffff88'
-          : 'repeating-linear-gradient(45deg, white, white 9px, #fafafa 9px, #fafafa 14px)',
+          : 'repeating-linear-gradient(45deg, white, white 9px, #fafafa 9px, #fafafa 14px)',*/
       'user-select': 'none',
       'z-index': 0,
       // Force a minimum width and height so areas are still visible when the
