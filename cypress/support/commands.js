253574ee7e64dd2f1a016670070a3399181cb48c
Gerardo Perrucci <centrodph@gmail.com>
2022-02-14 11:38:28 -0300

    Cypress testing: initial setup and first test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,17 +9,7 @@
 // ***********************************************
 //
 //
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const local = 'http://localhost:3000'
+Cypress.Commands.add('openApp', () => {
+  cy.visit(local)
+})
